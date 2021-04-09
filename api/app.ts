fbec2083725a293c379506fcf7159c5fc7c8533f
sroman215 <scott.andrew.roman@gmail.com>
2021-04-09 19:45:18 -0400

    Initial check
    Got the basic TS structure building and auto recompiling backend via nodemon.

---
+++ api/app.ts
@@ -0,0 +1,18 @@
+
+import * as express from "express";
+import path = require("path")
+
+const app = express()
+const port = 3000
+
+// API layer to hit via the frontend or via brower
+app.get('/api', (req, res) => {
+    res.send('Hello World BIG WORLD!')
+})
+
+// Default route used to serve the frontend
+app.use('/', express.static(path.join(__dirname, '/client/dist')));
+
+
+// Start listening on the port
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
